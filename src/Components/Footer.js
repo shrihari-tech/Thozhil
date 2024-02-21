254b2cb6b2395e247a74eac5fb6cd727f7056848
SHRIHARI BH <bhshrihari@gmail.com>
2024-02-21 19:47:24 +0530

    footer commit

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -10,10 +10,10 @@ function Footer(){
     
     
       const fields = [
-        { label: 'Name', field: 'name' },
-        { label: 'Email', field: 'email' },
-        { label: 'Phone', field: 'phone' },
-        { label: 'Comments', field: 'comments' }
+        { label: 'Name'},
+        { label: 'Email' },
+        { label: 'Phone' },
+        { label: 'Comments' }
       ];
 
       const openingHours = [
@@ -21,6 +21,11 @@ function Footer(){
         { day: 'Sunday', hours: 'Closed' }
       ];
     
+      const contactInfo = {
+        address: 'No.69, Arni Road, Cheyyar, Thiruvanamalai District - 604407',
+        phone: 'Ph - 04182222474',
+        email: 'Email - cheyyartic@gmail.com'
+      }; 
     return (
         <div className="fbg">
         <div className="footer">
@@ -51,8 +56,16 @@ function Footer(){
         ))}
         </div>
       </div>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15570.347127817191!2d79.5299588!3d12.6750766!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a52d73bba9ded13%3A0x5deb610544663ab8!2sCheyyar%20Industrial%20Training%20Institute%20(ITI)!5e0!3m2!1sen!2sin!4v1708524035754!5m2!1sen!2sin" className="location"></iframe>
+      <div className="contactus">
+        <h3>Contact Us</h3>
+        <p>{contactInfo.address}</p>
+        <p>{contactInfo.phone}</p>
+        <p>{contactInfo.email}</p>
+      </div>
       </div>
       <img src="footer-bg.png" alt="badges" className="footerbg"></img>
+      
     </div>
     );
 }
